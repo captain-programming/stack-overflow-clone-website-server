4fe3dd2386350dec08fed189fa851a7f662f050c
Dinesh Karde <dineshkarde103@gmial.com>
2023-01-23 13:20:58 +0530

    change

--- index.js
+++ index.js
@@ -8,11 +8,6 @@ import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
-
-const dbConnect=()=>{
-  return mongoose.connect(`${process.env.MONGODB_URL}`)
-}
-
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors());
@@ -24,10 +19,12 @@ app.get("/", (req, res) => {
 app.use('/users', userRoutes);
 app.use('/questions', questionRoutes);
 app.use('/answer', answerRoutes);
+
 const PORT = process.env.PORT || 8080;
 
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
-app.listen(PORT, async()=>{
-  await dbConnect();
-  console.log(`server started on port ${PORT}`);
-});
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`)))
+.catch((err) => console.log(err.message));
